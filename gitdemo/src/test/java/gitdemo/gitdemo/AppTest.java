841d35ce71baeee56f4aabba36b56f2b6c795f81
RPD001 <RPD001@pc10630.Corporate.S600605-AD01.corp>
2017-07-25 19:31:43 +0800

    Checking.....

---
+++ gitdemo/src/test/java/gitdemo/gitdemo/AppTest.java
@@ -0,0 +1,31 @@
+package gitdemo.gitdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+		protected WebDriver driver;
+		
+		@Test
+		public void gitTutorial() throws InterruptedException {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\RPD001\\Desktop\\Selenium\\chrome\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			String eTitle = "Google";
+			String aTitle = "";
+			
+			driver.get("http://google.com");
+			driver.manage().window().maximize();
+			aTitle = driver.getTitle();
+			//compare the actual title with the expected title
+			if(aTitle.contentEquals(eTitle)) {
+				System.out.println("Test Passed");
+			} else {
+				System.out.println("Test Failed");
+			}
+			driver.close();
+		}
+	
+}
+    
